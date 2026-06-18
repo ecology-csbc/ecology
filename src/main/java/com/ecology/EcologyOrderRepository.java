41f3823a560391997762647234296d7f749f4bdc
pavlovolexandr69 <91127015+pavlovolexandr69@users.noreply.github.com>
2026-06-18 11:20:39 +0000

    123

--- src/main/java/com/ecology/EcologyOrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.retail;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RetailOrderRepository extends MongoRepository<RetailOrder, String> {
-}
